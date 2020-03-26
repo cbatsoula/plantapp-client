00938065049f71adb84c52911d0459bc112150fd
cbatsoula <cbatsoula@gmail.com>
2020-03-26 15:22:00 -0400

    stuff and things

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
