6a935d461af491b489cbfa8a030ada16abaf02a7
cbatsoula <cbatsoula@gmail.com>
2020-03-27 14:57:50 -0400

    some notes and ideas - also in journal

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 class App extends React.Component {
 
   state = {
-    term: "sage"
+    term: "sage" //snake case for spaces fyi
   }
 
   componentDidMount () {
-    fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?q=${this.state.term}&token=${process.env.REACT_APP_TREFLE_API_KEY}`)
+
+    //scientific_name is way more accurate than trying to find what "sage" I meant as a common name -- include this in the FAQ/About/How to use
+    fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?q=${this.state.term}&page=2&token=${process.env.REACT_APP_TREFLE_API_KEY}`)
       .then( r => r.json())
       .then( data => {
         console.log("where are you", data)
