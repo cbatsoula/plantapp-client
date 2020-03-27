7dd13a374966804094c13c6314cf98ac141738cd
cbatsoula <cbatsoula@gmail.com>
2020-03-27 14:36:21 -0400

    fetch success

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 
-
 class App extends React.Component {
+
+  componentDidMount () {
+    fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?common_name=sage&token=${process.env.REACT_APP_TREFLE_API_KEY}`)
+      .then( r => r.json())
+      .then( data => {
+        console.log("where are you", data)
+      })
+  }
   render () {
     return (
       <div className="App">
