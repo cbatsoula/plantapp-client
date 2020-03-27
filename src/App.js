19b61b90933556961270674184a7d777cb2b028b
cbatsoula <cbatsoula@gmail.com>
2020-03-27 14:45:06 -0400

    cors error, working now

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import './App.css';
 
 class App extends React.Component {
 
+  state = {
+    term: "sage"
+  }
+
   componentDidMount () {
-    fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?common_name=sage&token=${process.env.REACT_APP_TREFLE_API_KEY}`)
+    fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?q=${this.state.term}&token=${process.env.REACT_APP_TREFLE_API_KEY}`)
       .then( r => r.json())
       .then( data => {
         console.log("where are you", data)
+      })
+      .catch(error => {
+        console.log("error", error)
       })
   }
   render () {
