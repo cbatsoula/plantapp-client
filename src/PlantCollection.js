978b0468ddb904faaeae7e69b982991dee72a0f8
cbatsoula <cbatsoula@gmail.com>
2020-04-01 17:10:51 -0400

    working

--- src/PlantCollection.js
+++ src/PlantCollection.js
@@ -4,11 +4,19 @@ import './App.css';
 
 class PlantCollection extends React.Component {
 
+  renderNames () {
+    // this.props.someData.map( el => {return <h1> el.common_name </h1>})
+  }
+
+
   render () {
-    console.log("in plant collection", this.props)
+    console.log("in plant collection", this.props.someData)
+
     return (
       <div className="PlantCollection">
-
+      {
+        this.renderNames()
+      }
       </div>
     );
   }
