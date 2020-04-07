b6dca79bd815cd9529769d4dee5eb2c9b5df449a
cbatsoula <cbatsoula@gmail.com>
2020-04-07 14:21:20 -0400

    rethink layout for displaying selected plant info -- a paneled view might be better suited for browsing

--- src/PlantShow.js
+++ src/PlantShow.js
@@ -5,7 +5,7 @@ import './App.css';
 
 class PlantShow extends React.Component {
 
-
+  //maybe change layout, so that the detail show page of each plant will almost just be a panel, I think this would be a good way to browse the plants with images rather than even showing the image in a PlantCard maybe??!!!
 
   render () {
 
