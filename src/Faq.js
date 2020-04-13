856ac5b58e5c7a599c42da3bc73473ce88af165d
cbatsoula <cbatsoula@gmail.com>
2020-04-13 13:13:01 -0400

    FAQ more notes

--- src/Faq.js
+++ src/Faq.js
@@ -13,10 +13,30 @@ class Faq extends React.Component {
   //About/why
   //gardening indoors, wanted to make a web app so I can browse different plants and see what their needs are so I can make better choices on which plants to care for, I've killed poppies and lavender on my first attempt of having plants and that has lead me to drawing diagrams of my living space with details on temp and sun
 
+  //Informational guide?
+  //sun     - morning
+  //        - evening
+  //        - full/partial/indirect
+  //        - measuring sun/seasonal changes
+
+  //water   - how often to water?
+  //        - measuring moisture / plant needs
+  //        - signs of underwatering
+  //        - signs of overwatering
+
+  //repotting - signs from plant
+  //          - seasonally
+  //
+
+  //hardiness zones - outdoor
+  //                - indoor
+
+
   //Tips for planning a garden
-  //draw out your space, and take a day or two with sun to mark out where the sun hits, what times you get sun (morning/evening sun differs), what direction your windows/space face,
+  //draw out your space, and take a day or two with sun to mark out where the sun hits, what times you get sun (morning/evening sun differs), what direction your windows/space face, ex. full evening sun
   //a gardening tool to measure sun, ph, and dirt moisture levels is v nice to have, they are not expensive on amazon generally. //link one..??
-  
+  // moving plants as the day continues, and moving plants for seasons?
+
 
   render () {
 
