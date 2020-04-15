c1a3940f8e204647c9797e65ae5dba0381245715
cbatsoula <cbatsoula@gmail.com>
2020-04-15 13:58:40 -0400

    filtering search with api fetch request filtering

--- src/App.js
+++ src/App.js
@@ -31,9 +31,6 @@ class App extends React.Component {
   selectPlant = (props) => {
     console.log("HELL YEAH", props)
     console.log("why", this.state)
-    // this.setState({
-    //   currentBeach: selectedBeach
-    // }, () => {this.props.history.push('/beach')})
     this.setState({
       currentPlant: props,
       searchTerm: null,
@@ -80,6 +77,10 @@ class App extends React.Component {
 
   }
 
+  handleColorChange(event) {
+   this.setState({value: event.target.value});
+ }
+
   render () {
     console.log("here it is from APP", this.state)
     return (
@@ -89,6 +90,20 @@ class App extends React.Component {
       handleSearchSubmit={this.handleSearchSubmit}
       handleSearchChange={this.handleSearchChange}
       searchTerm={this.state.searchTerm} />
+      <label>
+          Fruit color:
+          <select value={this.state.value} onChange={this.handleColorChange}>
+            <option value="White">White</option>
+            <option value="Yellow">Yellow</option>
+            <option value="Orange">Orange</option>
+            <option value="Red">Red</option>
+            <option value="Purple">Purple</option>
+            <option value="Blue">Blue</option>
+            <option value="Green">Green</option>
+            <option value="Brown">Brown</option>
+            <option value="Black">Black</option>
+          </select>
+        </label>
       {
         this.state.plantdata
         ?
