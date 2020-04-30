434926e6357f786d4efe243e8756e7d503277a13
cbatsoula <cbatsoula@gmail.com>
2020-04-30 16:26:43 -0400

    formatting on cards

--- src/GardenCard.js
+++ src/GardenCard.js
@@ -10,9 +10,11 @@ class GardenCard extends React.Component {
     console.log("Garden Card Props:", this.props)
     return (
       <div className="GardenCard">
-        <span><h3> Name: {this.props.plant.plant_name}</h3><br />
-        <h3> Nickname: {this.props.plant.plant_nickname}</h3><br />
-        <h3> Acquired on: {this.props.plant.acquired}</h3><br /></span>
+        <div className="GardenCardDetails">
+          <span><h3> Name: {this.props.plant.plant_name}</h3><br />
+          <h3> Nickname: {this.props.plant.plant_nickname}</h3><br />
+          <h3> Acquired on: {this.props.plant.acquired}</h3><br /></span>
+        </div>
       </div>
     );
   }
