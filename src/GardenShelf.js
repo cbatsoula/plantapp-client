b6d8bb219a2b6c8c194c0c8adfe8e6e63eece490
cbatsoula <cbatsoula@gmail.com>
2020-04-30 15:49:20 -0400

    garden shelf, wip cards for plants coming, can't reuse plantCards from show

--- src/GardenShelf.js
+++ src/GardenShelf.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 
 
+
 class GardenShelf extends React.Component {
+
+  listPlants = () => {
+    this.props.myPlants.map( plant => {console.log(plant.plant_name)})
+  }
 
   render () {
     console.log("Garden Shelf Props:", this.props)
     return (
       <div className="GardenShelfContainer">
         <h3>These are your plants!</h3>
+        {this.listPlants()}
       </div>
     );
   }
