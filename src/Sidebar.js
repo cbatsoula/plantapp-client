31192e6fb38cce4d58ea9fdd1758d7bf9d6b2bc8
cbatsoula <cbatsoula@gmail.com>
2020-05-06 14:45:28 -0400

    no internet, wip on style and name

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -53,8 +53,8 @@ class Sidebar extends React.Component {
 
           <Link to="/faq"><button style={{textDecoration: 'none'}}>Faq</button></Link>
 
-          <button>Sign In</button>
-          <button>Sign Up</button>
+          <span><Link to="/signin" ><button style={{textDecoration: 'none'}}>Sign In</button></Link>
+          <Link to="/signup" ><button style={{textDecoration: 'none'}}>Sign Up</button></Link></span>
 
         </Router>
       </div>
