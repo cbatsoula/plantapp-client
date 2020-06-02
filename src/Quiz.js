bcc0fad069d340dbd42769ce8f0275e75a1dab3f
cbatsoula <cbatsoula@gmail.com>
2020-06-02 16:45:55 -0400

    added Quiz class component

---
+++ src/Quiz.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+class Quiz extends React.Component {
+
+  state = {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    console.log("QUIZ", this.props)
+    return (
+      <>
+      <div className="QuizContainer">
+      
+      </div>
+      </>
+
+    )
+  }
+}
+export default Quiz;
